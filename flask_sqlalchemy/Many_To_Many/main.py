b4eab02c8ee5a66062b07c528c6bb79d15ca9e4d
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-08-21 16:15:36 +0100

    Many-To-Many relationship

--- flask_sqlalchemy/Many_To_Many/main.py
+++ flask_sqlalchemy/Many_To_Many/main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onetoone.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manytomany.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)

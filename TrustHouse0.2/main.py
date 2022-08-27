b72359f1f98e8b6d1d43882bf563ff13c886ffba
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-08-27 13:16:30 +0100

    Trust House 0.2 landing page, home page, write review page & base template configured; basic flask web routes set up

---
+++ TrustHouse0.2/main.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from flask import render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def landingpage():
+    return render_template("landingPage.html")
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/writeReview")
+def writeReview():
+    return render_template('writeReviewPage.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

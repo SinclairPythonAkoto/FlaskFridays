6ec0caf80bd1d6dbbbd5e872b0bf37f34786f2ea
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-09-12 12:56:31 +0100

    create address function applied

--- TrustHouse0.4.2/trusthouse/routes/upload_address.py
+++ TrustHouse0.4.2/trusthouse/routes/upload_address.py
@@ -4,6 +4,7 @@ from trusthouse.models.maps import Maps
 from trusthouse.utils.validate_postcode import validate_postcode_request
 from trusthouse.utils.validate_door import validate_door_request
 from trusthouse.utils.get_coordinates import get_postcode_coordinates
+from trusthouse.utils.create_address import create_new_address
 from trusthouse.utils.request_messages import warning_message, error_message, ok_message
 from flask import render_template, request, jsonify
 from ..extensions import app, db
@@ -29,25 +30,19 @@ class UploadAddress(MethodView):
         # if there is no preexisting postcode create new address.
         if postcode_request == False:
             # write fuction
-            new_address = Address(
-                door_num=door.lower(),
-                street=street_name.lower(),
-                location=location.lower(),
-                postcode=postcode.lower(),
-            )
-            db.session.add(new_address)
-            db.session.commit()
+            new_address = create_new_address(door, street_name, location, postcode)
             # write fuction
             user_postcode_coordinates = get_postcode_coordinates(postcode)
             # write fuction
-            if user_postcode_coordinates == None:
+            if user_postcode_coordinates == []:
                 # write fuction
                 data = {
                     'Incomplete upload': warning_message()[0], 
                     'Status':warning_message()[1]
                 }
                 return jsonify(data)
-            elif user_postcode_coordinates != None:
+            elif user_postcode_coordinates != []:
+                print(user_postcode_coordinates)
                 latitude = user_postcode_coordinates[0].get('lat')
                 longitude = user_postcode_coordinates[0].get('lon')
                 new_geo_map = Maps(
@@ -67,14 +62,7 @@ class UploadAddress(MethodView):
                 return jsonify(data)
         else:
             if door_request == False and postcode_request == True:
-                new_address = Address(
-                    door_num=door.lower(),
-                    street=street_name.lower(),
-                    location=location.lower(),
-                    postcode=postcode.lower(),
-                )
-                db.session.add(new_address)
-                db.session.commit()
+                new_address = create_new_address(door, street_name, location, postcode)
                 user_postcode_coordinates = get_postcode_coordinates(postcode)
                 # if there is an existing latitude & longitude
                 if user_postcode_coordinates:
--- TrustHouse0.4.2/trusthouse/utils/create_address.py
+++ TrustHouse0.4.2/trusthouse/utils/create_address.py
@@ -1,7 +1,7 @@
 from trusthouse.models.address import Address
 from ..extensions import db
 
-'''
+
 def create_new_address(door, street, location, postcode):
     """
     Creates a new address, storing it in the Address table
@@ -15,34 +15,3 @@ def create_new_address(door, street, location, postcode):
     db.session.add(new_address)
     db.session.commit()
     return new_address
-
-
-get_address_object = create_new_address(door,street,location,postcode)
-
-
-def address_object():
-    return create_new_address()
-'''
-
-class CreateNewAddress:
-
-    def __init__(self, door, street, location, postcode, new_address):
-        self.door = door
-        self.street = street
-        self.location = location
-        self.postcode = postcode
-        self.new_address = new_address
-
-        
-    
-    new_address = Address(
-        door_num=door.lower(),
-        street=self.street.lower(),
-        location=location.lower(),
-        postcode=postcode.lower(),
-    )
-    db.session.add(new_address)
-    db.session.commit()
-
-    get_address = 
-    

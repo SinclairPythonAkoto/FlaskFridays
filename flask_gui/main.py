836f451edc8fbd3da2afdfe7ae0351d71b568455
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-11-05 21:07:11 -0400

    Flask GUI app demo

---
+++ flask_gui/main.py
@@ -0,0 +1,28 @@
+from distutils.log import debug
+from flask import Flask  
+from flask import render_template
+from flaskwebgui import FlaskUI
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello():  
+    return render_template('index.html')
+
+@app.route("/home", methods=['GET'])
+def home(): 
+    return render_template('some_page.html')
+
+
+if __name__ == "__main__":
+
+    debug = False
+
+    if debug:
+        app.run(debug=True)
+    else:
+        FlaskUI(app, width=500, height=500, start_server="flask").run() 
+    
+   

2f32e8c220a0e021e011b9e66a179f5ded5bca0f
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-11-11 09:05:18 -0500

    2nd commit, db model(s) added

--- sports_gui/main.py
+++ sports_gui/main.py
@@ -1,10 +1,25 @@
-from distutils.log import debug
 from flask import Flask  
-from flask import render_template
+from flask import render_template, request, url_for
 from flaskwebgui import FlaskUI
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+# initialise the database
+app.config['SECRET_KEY'] = 'somepassword'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sportsgui.db'
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = True
+# creating database intsance
+db = SQLAlchemy(app)
+
+# create the db tables
+class NewSession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session_name = db.Column(db.String(50), nullable=False)
+    student_name = db.Column(db.String(30), nullable=False)
+    student_score = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
 
 @app.route("/")
@@ -14,6 +29,21 @@ def hello():
 @app.route("/home", methods=['GET'])
 def home(): 
     return render_template('some_page.html')
+
+@app.route("/new_session", methods=['GET', 'POST'])
+def new_session():
+    if request.method == 'GET':
+        return render_template('new_session.html')
+    else:
+        return "Your data has been sent"
+
+@app.route("/view_table")
+def view_table():
+    return "View data table"
+
+@app.route("/view_stats")
+def view_stats():
+    return "View stats & graphs from data tables"
 
 
 if __name__ == "__main__":
